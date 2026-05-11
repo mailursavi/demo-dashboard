2378af37d18a610a28f5b40489b6f36074d91fbc
mailursavi <mailursavi@gmail.com>
2026-05-11 12:40:57 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -36,7 +36,7 @@ st.caption("Interactive CGM, insulin, meal, activity, sleep, predictive and pres
 # =========================
 @st.cache_data
 def load_data():
-    df = pd.read_csv("cleaned_hupa_diabetes_recent (1)(1).csv")
+    df = pd.read_csv("cleaned_hupa_diabetes_recent (1).xlsb")
     demo = pd.read_csv("cleaned_demographics(1).csv")
 
     df["time"] = pd.to_datetime(df["time"], errors="coerce")
